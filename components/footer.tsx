47f572a6fb2762c0b4826c92637b72438d4ee50c
oMiguelwnl <contato.miguel.almeida7@gmail.com>
2025-05-16 10:02:45 -0300

    feat: add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-400">
+        &copy; 2025 Miguel. Todos direitos reservados.
+      </small>
+    </footer>
+  );
+}
