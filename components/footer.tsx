c6251ffeba4446de9079ba1c9d08285dd03b82c7
oMiguelwnl <contato.miguel.almeida7@gmail.com>
2025-05-16 11:09:29 -0300

    fix: correct name in footer copyright notice

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-400">
-        &copy; 2025 Miguel. Todos direitos reservados.
+        &copy; 2025 Miguel Rafael. Todos direitos reservados.
       </small>
     </footer>
   );
